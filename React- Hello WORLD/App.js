99efb81733d784488092a87d4cecc50f3e96b033
Jayant8621 <Jayant.8621@gmail.com>
2023-03-23 00:17:45 +0530

    No changes

--- React- Hello WORLD/App.js
+++ React- Hello WORLD/App.js
@@ -10,6 +10,7 @@
  *          <h2>H2 tag in child</h2>
  *      </div>
  *  </div>
+ * 
  * ReactElement(Obbject) ==>> HTML(Browser understands) 
  */
 const heading1 = React.createElement("h1", {}, "H1 tag in child");
@@ -17,9 +18,9 @@ const heading2 = React.createElement("h2", {}, "H2 tag in child");
 
 const child = React.createElement("div", {id:"child"}, [heading1, heading2]);
 const child2 = React.createElement("div", {id:"child2"}, [heading1, heading2]);
-const parent = React.createElement("div", {id:"parent"}, [child, child2]);
+const parent1 = React.createElement("div", {id:"parent"}, [child, child2]);
 
 
 //const heading1 = React.createElement("h1", { id:"heading", xyz: "123" }, "Hello World from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(parent1);
